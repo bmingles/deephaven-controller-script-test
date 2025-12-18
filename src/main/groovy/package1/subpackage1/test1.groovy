05514ed7c9cac90af9694880692dac44b891ef20
Arman Farhangi <armanfarhangi@deephaven.io>
2025-12-18 14:48:42 -0800

    Try imports with full class names

--- src/main/groovy/package1/subpackage1/test1.groovy
+++ src/main/groovy/package1/subpackage1/test1.groovy
@@ -1,5 +1,7 @@
 package package1.subpackage1
 
+import package2.subpackage1.test1
+
 static void test() {
     println("Controller sourced package1.subpackage1.test1")
     package2.subpackage1.test1.test()
--- src/main/groovy/package2/subpackage1/test1.groovy
+++ src/main/groovy/package2/subpackage1/test1.groovy
@@ -1,5 +1,7 @@
 package package2.subpackage1
 
+import package2.subpackage1.test2
+
 static void test() {
     println("Controller sourced package2.subpackage1.test1")
     package2.subpackage1.test2.test()
